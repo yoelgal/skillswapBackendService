b7ddd288ee880828e5f7bb3792d0f8eb6ce33131
yoelgal <yoelgal108@gmail.com>
2024-01-04 13:28:26 +0000

    feat: Added real courses to seeding

--- seed/user.seed.ts
+++ seed/user.seed.ts
@@ -1,3 +1,5 @@
+import { courses } from '../src/utils/courses.data';
+
 function getFirstNameFromEmail(email: string): string {
   return email[0].toUpperCase() + email.slice(1).split('.')[0];
 }
@@ -72,15 +74,28 @@ function generateRandomEmail(): string {
   return `${firstName}.${lastName}@${domain}`;
 }
 
+function pickRandomCourse(): string {
+  let course: string;
+  while (true) {
+    course = courses[Math.floor(Math.random() * courses.length)];
+    if (
+      course !== 'UNDERGRADUATE COURSES' &&
+      course !== 'POSTGRADUATE COURSES'
+    ) {
+      break;
+    }
+  }
+  return course;
+}
+
 function generateRandomUser(): any {
   const email = generateRandomEmail();
   const name = getFirstNameFromEmail(email);
   const password = 'defaultPassword'; // You might want to generate a more secure password
   const age = Math.floor(Math.random() * 10) + 18; // Random age between 18 and 48
   const gender = Math.floor(Math.random() * 2) + 1; // Random gender 1 or 2
-  const yearOfStudy = Math.floor(Math.random() * 5) + 1; // Random start year between 2020 and 2023
-  const courses = ['Course 1', 'Course 2', 'Course 3'];
-  const course = courses[Math.floor(Math.random() * courses.length)];
+  const yearOfStudy = Math.floor(Math.random() * 5) + 1; // Random start year between 2020 and 2023;
+  const course = pickRandomCourse();
   const reports = Math.floor(Math.random() * 10); // Random number of reports between 0 and 9
 
   return {
